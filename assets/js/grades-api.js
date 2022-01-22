b8b7de1bb55b9941b832a447913e294edecf40b8
Asad Dhorajiwala <asadrehandhorajiwala@gmail.com>
2022-01-21 22:33:17 -0800

    Updated ubcgrades API endpoint

--- assets/js/grades-api.js
+++ assets/js/grades-api.js
@@ -1,26 +1,27 @@
-(function() {
-  function loadGrades(subject, course) {
-    return fetch(`https://ubcgrades.com/api/course-profile/${subject}/${course}`)
-      .then(res => res.json());
+(async () => {
+  async function loadGrades(subject, course) {
+    const res = await fetch(`https://ubcgrades.com/api/v2/course-statistics/UBCV/${subject}/${course}`);
+    return await res.json();
   }
+
   function addDescription(list, term, details) {
     const dt = document.createElement('dt');
     const dd = document.createElement('dd');
-    dt.className = 'col-sm-3'
+    dt.className = 'col-sm-3';
     dt.textContent = term;
-    dd.className = 'col-sm-9'
+    dd.className = 'col-sm-9';
     dd.textContent = details + '%';
     list.append(dt, dd);
   }
-  document.querySelectorAll('.grades-api').forEach(function(gradesEl) {
-    console.log(gradesEl);
-    loadGrades(gradesEl.dataset.subject, gradesEl.dataset.course).then(json => {
-      const fragment = document.createDocumentFragment();
-      addDescription(fragment, 'Average', json.average);
-      addDescription(fragment, 'Highest Grade', json.high);
-      addDescription(fragment, 'Lowest Grade', json.low);
-      addDescription(fragment, 'Percentage passed', json.pass_percent);
-      gradesEl.append(fragment);
-    });
-  });
+
+  const gradesEl = document.getElementsByClassName('grades-api')[0];
+  const grades = await loadGrades(gradesEl.dataset.subject, gradesEl.dataset.course);
+  const { average, average_past_5_yrs, max_course_avg, min_course_avg, stdev } = grades;
+  const fragment = document.createDocumentFragment();
+  addDescription(fragment, 'Average (all time)', average);
+  addDescription(fragment, 'Average (past 5 years)', average_past_5_yrs);
+  addDescription(fragment, 'Highest Grade', max_course_avg);
+  addDescription(fragment, 'Lowest Grade', min_course_avg);
+  addDescription(fragment, 'Standard Deviation', stdev);
+  gradesEl.append(fragment);
 })();
